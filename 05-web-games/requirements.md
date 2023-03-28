1c3fb29e664d15820e706d66bb03c81a1272d267
imranpathan8107 <imran.pathan.8202@gmail.com>
2023-03-28 22:26:13 +0530

    enemy using canvas

---
+++ 05-web-games/requirements.md
@@ -0,0 +1,21 @@
+# 2d canvas game
+
+## Game Features (required)
+
+1. 1 player (blue box)
+2. 1 enemy moving up and down(red box)
+3. press ⬅➡ button on keyboard to movethe player [hint: add event listner for keydown and keyup]
+4. 1 goal at the right side of the box
+5. When player and enemy collide, game over
+6. When Player reached goal, level complete 
+7. every level increase speed of enemy
+
+## Game Features (recommended)
+1. 1 player (mario image)  [hint: sprite instead of fillrect]
+sprite can draw png object instead of rect
+2. 2 enemy moving up and down at different speeds(mario turtle image)
+3. press ⬅➡ button on keyboard to movethe player [hint: add event listner for keydown and keyup]
+4. 1 goal at the right side of the box (image of mario mushroom)
+5. When player and enemy collide, game over
+6. When Player reached goal, level complete
+7. every level increase speed of enemy
--- 05-web-games/script.js
+++ 05-web-games/script.js
@@ -1,5 +1,68 @@
 let canvas = document.getElementById('canvas')
 let context = canvas.getContext('2d');  //webgl context can be used to create 3d games
+
+let enemy = {
+    color:'red',
+    x: 100,
+    y: 0,
+    h: 50,
+    w: 50,
+    vx:0,
+    vy:1
+}
+
+let player = {
+    color:'blue',
+    x: 0,
+    y: 175,
+    h: 50,
+    w: 50,
+    vx:3,
+    vy:0
+}
+// drawBox function is used to draw the player and enemy
+function drawBox(box){
+    context.fillStyle = box.color;
+    context.fillRect(box.x, box.y, box.w, box.h);
+}
+
+function updateGameState(){
+    // update the enemy position
+    enemy.y += enemy.vy;
+    // check if the enemy is out of the canvas
+    if(enemy.y+enemy.h > canvas.height){    //bottom of enemy is hitting bottom of canvas, reverse the velocity
+        enemy.vy = -enemy.vy;
+    }
+    else if(enemy.y < 0){   //top of enemy is hitting top of canvas, reverse the velocity
+        enemy.vy = -enemy.vy;
+    }
+    player.x += player.vx;
+    if(player.x+player.w > canvas.width){    //right of player is hitting right of canvas, reverse the velocity
+        player.vx = -player.vx;
+        // enemy.vy = 1.1*enemy.vy;
+    }else if(player.x < 0){   //left of player is hitting left of canvas, reverse the velocity
+        player.vx = -player.vx;
+    }
+}
+
+function updateGame(){
+    //update game state
+    updateGameState();
+    // clean the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // draw the player
+    drawBox(player);
+    // draw the enemy
+    drawBox(enemy);
+
+    window.requestAnimationFrame(updateGame);
+
+}
+
+updateGame();
+
+
+/* basic code:
 // context is a way to paint thing on the canvas, we can use context object to do things like fill a rectangle
 
 context.fillStyle = 'brown';
@@ -11,9 +74,25 @@ context.clearRect(0,0,canvas.width,canvas.height);
 
 let posX = 0;
 let posY = 0;
-setInterval(function(){
-    posX+=10;
-    posY+=10;
-    context.clearRect(0,0,canvas.width,canvas.height);
+let speed = 1.5;   // speed of the rectangle
+
+context.fillRect(10,10,300,300);
+context.clearRect(20,20,30,30);
+
+// setInterval(function(){
+//     posX+=10;
+//     posY+=10;
+//     context.clearRect(0,0,canvas.width,canvas.height);   // clear everything from 0,0 to width and height of canvas
+//     context.fillRect(posX,posY,20,20);
+// },2000);
+
+function drawNext(){
+    posX+=speed;
+    posY+=speed;
+    context.clearRect(0,0,canvas.width,canvas.height);   // clear everything from 0,0 to width and height of canvas
     context.fillRect(posX,posY,20,20);
-},2000) 
+    window.requestAnimationFrame(drawNext);
+}
+
+drawNext();
+*/
