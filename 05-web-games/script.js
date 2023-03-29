22a71b23bc31ee56a46200cb9c28d1aed5d4f8e2
imranpathan8107 <imran.pathan.8202@gmail.com>
2023-03-29 20:35:30 +0530

    canvas basic game complete

--- 05-web-games/script.js
+++ 05-web-games/script.js
@@ -1,14 +1,24 @@
 let canvas = document.getElementById('canvas')
 let context = canvas.getContext('2d');  //webgl context can be used to create 3d games
 
-let enemy = {
+let enemy1 = {
     color:'red',
-    x: 100,
+    x: 250,
     y: 0,
     h: 50,
     w: 50,
     vx:0,
     vy:1
+}
+
+let enemy2 = {
+    color:'red',
+    x: 500,
+    y: 0,
+    h: 50,
+    w: 50,
+    vx:0,
+    vy:1.5
 }
 
 let player = {
@@ -17,31 +27,97 @@ let player = {
     y: 175,
     h: 50,
     w: 50,
-    vx:3,
+    vx:0.1,
     vy:0
 }
+
+let goal = {
+    color:'green',
+    x: context.canvas.width-50,
+    y: 175,
+    h: 50,
+    w: 50,
+    vx:0,
+    vy:0
+}
+
 // drawBox function is used to draw the player and enemy
 function drawBox(box){
     context.fillStyle = box.color;
     context.fillRect(box.x, box.y, box.w, box.h);
 }
 
+
+function drawImg() {
+    // const ctx = document.getElementById("canvas").getContext("2d");
+    const img = new Image();
+    img.onload = () => {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 3; j++) {
+          context.drawImage(img, j * 50, i * 38, 50, 38);
+        }
+      }
+    };
+    img.src = "mario.jpg";
+}
+
+function collisin(box1,box2){
+    return box1.x < box2.x + box2.w &&
+    box1.x + box1.w > box2.x &&
+    box1.y < box2.y + box2.h &&
+    box1.y + box1.h > box2.y;
+}
+
 function updateGameState(){
-    // update the enemy position
-    enemy.y += enemy.vy;
-    // check if the enemy is out of the canvas
-    if(enemy.y+enemy.h > canvas.height){    //bottom of enemy is hitting bottom of canvas, reverse the velocity
-        enemy.vy = -enemy.vy;
+    // update the enemy1 position
+    enemy1.y += enemy1.vy;
+    // check if the enemy1 is out of the canvas
+    if(enemy1.y+enemy1.h > canvas.height){    //bottom of enemy1 is hitting bottom of canvas, reverse the velocity
+        enemy1.vy = -enemy1.vy;
     }
-    else if(enemy.y < 0){   //top of enemy is hitting top of canvas, reverse the velocity
-        enemy.vy = -enemy.vy;
+    else if(enemy1.y < 0){   //top of enemy1 is hitting top of canvas, reverse the velocity
+        enemy1.vy = -enemy1.vy;
     }
-    player.x += player.vx;
+
+    enemy2.y += enemy2.vy;
+    // check if the enemy2 is out of the canvas
+    if(enemy2.y+enemy2.h > canvas.height){    //bottom of enemy2 is hitting bottom of canvas, reverse the velocity
+        enemy2.vy = -enemy2.vy;
+    }
+    else if(enemy2.y < 0){   //top of enemy2 is hitting top of canvas, reverse the velocity
+        enemy2.vy = -enemy2.vy;
+    }
+
+
+    // player.x += player.vx;
+    document.addEventListener('keydown', function(event){
+        if(event.key == 'ArrowLeft'){
+            player.x -= player.vx;
+        }
+        else if(event.key == 'ArrowRight'){
+            player.x += player.vx;
+        }
+    });
     if(player.x+player.w > canvas.width){    //right of player is hitting right of canvas, reverse the velocity
-        player.vx = -player.vx;
-        // enemy.vy = 1.1*enemy.vy;
+        player.x = 0;
     }else if(player.x < 0){   //left of player is hitting left of canvas, reverse the velocity
-        player.vx = -player.vx;
+        player.x = 0;
+    }
+
+    if(collisin(player,enemy1) || collisin(player,enemy2)){
+        console.log('collision with enemy, You Lost!');
+        player.x = 0;
+    }
+    if(collisin(player,goal)){
+        console.log('you win');
+        player.x = 0;
+        player.y = 175;
+        if(enemy1.vy>=-3 && enemy1.vy<=3){
+            enemy1.vy = 1.05*enemy1.vy;
+        }
+        if(enemy2.vy>=-5 && enemy2.vy<=5){
+            enemy2.vy = 1.1*enemy2.vy;
+        }
     }
 }
 
@@ -52,12 +128,18 @@ function updateGame(){
     context.clearRect(0, 0, canvas.width, canvas.height);
     // draw the player
     drawBox(player);
+    drawImg();
+    drawBox(goal);
+
     // draw the enemy
-    drawBox(enemy);
+    drawBox(enemy1);
+    drawBox(enemy2);
 
     window.requestAnimationFrame(updateGame);
 
 }
+
+
 
 updateGame();
 
